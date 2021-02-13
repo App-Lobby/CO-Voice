57aacffb55f05cd61d4b4e6af6e14a107a66ccc4
myawesomehub <mdcode2021@gmail.com>
2021-02-13 20:21:20 +0530

    Initial Commit

---
+++ CO-Voice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CO-Voice
+//
+//  Created by Mohammad Yasir on 13/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
